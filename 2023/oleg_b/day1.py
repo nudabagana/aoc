5aa8ca03f6667857183252348d17b155b982d980
Oleg Bourdo <oleg.bourdo@revelsystems.com>
2023-12-04 11:11:51 +0200

    Oleg day 1 Trebuchet adventure task

---
+++ 2023/oleg_b/day1.py
@@ -0,0 +1,20 @@
+'''
+the elf text should be inside the file "elf_text.txt"
+the calibration number printed at the code finish
+'''
+
+import re
+
+
+elf_file = 'elf_text.txt'
+calibration_val = 0
+
+with open(elf_file) as elf_txt:
+    for line in elf_txt.readlines():
+        re.search(r'\d+', line).group(0)
+        first_number = int(re.search(r'\d+', line).group(0)) * 10
+        last_number = int(re.search(r'\d+', line[::-1]).group(0))
+        full_number = first_number + last_number
+        calibration_val += full_number
+
+print(calibration_val)
